cb9f2b253175a81b9cfb229a85ad4d4fad5d4b5c
ricardo.castilho <r.m.n.castilho@gmail.com>
2025-01-30 19:29:13 +0000

    update tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 import Image from "next/image";
 import Hello from "@/app/components/hello";
 
-export default function Home() {
-  console.log("what am I doing here? --SEVER/CLIENT?");
+const Home = () => {
   return (
-    <>
-      <h1 className="text-3xl font-bolt underline">Welcome to next.js</h1>;
-      <Hello />
-    </>
+    <h1 className="text-3xl text-white font-black">
+      Welcome to the world of Next.js
+    </h1>
   );
-}
+};
+
+export default Home;
