97c6ef09b3563a43ff5cd036954b723ec9dbb965
ricardo.castilho <r.m.n.castilho@gmail.com>
2025-01-30 20:40:48 +0000

    ESlint & Prettier Setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,7 @@
-import Image from "next/image";
-import Hello from "@/app/components/hello";
-
-const Home = () => {
+export default function Home() {
   return (
-    <h1 className="text-3xl text-white font-black">
+    <h1 className="text-3xl  text-white font-black">
       Welcome to the world of Next.js
     </h1>
   );
-};
-
-export default Home;
+}
